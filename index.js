dd465521dfbb4133f9e33aa87b565c739d0156b3
MokshaSri <moksha.srinivas@gmail.com>
2017-08-12 09:49:29 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function onclickstart() {
 	window.location.hash = "jumppage2";
 };
+function onclicksaltybutton() {
+	window.location.hash = "jumppage3"
+};
+function onclicksarkybutton() {
+	window.location.hash = "jumppage4"
+};
+function onclickwholesomebutton() {
+	window.location.hash = "jumppage5"
+};
